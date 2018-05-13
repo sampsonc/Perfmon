3e0b97b00a80100fde3dd0ff0304ab21f6577fe7
Carl Sampson <csampson@c02v38k9htd6.indy.rr.com>
2018-05-13 00:16:23 -0400

    Initial Commit

---
+++ src/burp/BurpExtender.java
@@ -0,0 +1,47 @@
+package burp;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import perfmon.PerfComponent;
+import perfmon.PerfTab;
+
+/**
+ *
+ * @author csampson
+ */
+public class BurpExtender implements IBurpExtender
+{
+    private IExtensionHelpers helpers;
+    private static final String EXTENSION_NAME = "Perfmon";
+    private OutputStream os;
+    
+    @Override
+    public void registerExtenderCallbacks(IBurpExtenderCallbacks callbacks)
+    {     
+        helpers = callbacks.getHelpers();  //Helper functions available to use
+        callbacks.setExtensionName(EXTENSION_NAME);  //Set the extension name
+        os = callbacks.getStdout();  //Handle to the outputstream
+        
+        //Add UI
+        PerfTab tab = new PerfTab("Perfmon", callbacks);
+        PerfComponent comp = new PerfComponent(callbacks, this);
+        tab.addComponent(comp);
+        comp.start();
+                
+        println("Perfmon Loaded");
+    }
+    
+    public void println(String msg)
+    {
+        try
+        {
+            os.write(msg.getBytes());
+        } catch (IOException ex)
+        {
+            Logger.getLogger(BurpExtender.class.getName()).log(Level.SEVERE, null, ex);
+        } 
+    }
+    
+}
---
+++ src/perfmon/PerfComponent.java
@@ -0,0 +1,117 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2017 Carl Sampson <chs@chs.us>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package perfmon;
+
+import burp.BurpExtender;
+import burp.IBurpExtenderCallbacks;
+import java.awt.GridLayout;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Carl Sampson <chs@chs.us>
+ */
+public class PerfComponent extends JPanel implements Runnable
+{
+
+    IBurpExtenderCallbacks callbacks;
+    BurpExtender extender;
+    JLabel lblThread;
+    JLabel lblThreadValue;
+    JLabel lblTotalMemory;
+    JLabel lblTotalMemoryValue;
+    JLabel lblUsedMemory;
+    JLabel lblUsedMemoryValue;
+
+    private Thread t;
+
+    public PerfComponent(IBurpExtenderCallbacks callbacks, BurpExtender extender)
+    {
+        this.callbacks = callbacks;
+        this.extender = extender;
+        t = null;
+        initComponents();
+    }
+
+    private void initComponents()
+    {
+       lblThread = new JLabel("Threads: ");
+       callbacks.customizeUiComponent(lblThread);
+       
+       lblThreadValue = new JLabel("0");
+       callbacks.customizeUiComponent(lblThreadValue);
+       
+       lblUsedMemory = new JLabel("Memory Used: ");
+       callbacks.customizeUiComponent(lblUsedMemory);
+       
+       lblUsedMemoryValue = new JLabel("0");
+       callbacks.customizeUiComponent(lblUsedMemoryValue);
+       
+       lblTotalMemory = new JLabel("Total Memory: ");
+       callbacks.customizeUiComponent(lblTotalMemory);
+       
+       lblTotalMemoryValue = new JLabel("0");
+       callbacks.customizeUiComponent(lblTotalMemoryValue);
+
+       GridLayout layout = new GridLayout(0, 2);
+       this.setLayout(layout);
+       
+       this.add(lblThread);
+       this.add(lblThreadValue);
+       this.add(lblUsedMemory);
+       this.add(lblUsedMemoryValue);
+       this.add(lblTotalMemory);
+       this.add(lblTotalMemoryValue);
+    }
+
+    @Override
+    public void run()
+    {
+        while (true)
+        {
+            try
+            {
+                Set<Thread> threadSet = Thread.getAllStackTraces().keySet();
+                lblThreadValue.setText(Integer.toString(threadSet.size()));
+                lblTotalMemoryValue.setText(String.format("%,d", Runtime.getRuntime().totalMemory()));
+                lblUsedMemoryValue.setText(String.format("%,d", Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()));
+                Thread.sleep(5000);
+            } catch (InterruptedException ex)
+            {
+                Logger.getLogger(PerfComponent.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+    
+     public void start () {
+      if (t == null) {
+         t = new Thread (this, "Thread");
+         t.start ();
+      }
+   }
+}
